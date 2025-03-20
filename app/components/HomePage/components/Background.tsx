e5c9eea3f91b0fb59027acf02ddc8f3a079848fa
edeldev <edeldev25@gmail.com>
2025-03-20 13:29:13 -0600

    chore:changeVideo

--- app/components/HomePage/components/Background.tsx
+++ app/components/HomePage/components/Background.tsx
@@ -1,15 +1,16 @@
-import Image from "next/image";
-
 export const Background = () => {
   return (
     <div className="fixed inset-0 z-[-1] overflow-hidden min-h-screen">
-      <Image
-        src="/images/background-sinfondo.png"
-        alt="background"
-        width={100}
-        height={100}
-        className="w-full h-full object-cover"
-      />
+      <video
+        autoPlay
+        muted
+        loop
+        playsInline
+        className="fixed top-0 left-0 w-full h-full object-cover -z-[1]"
+      >
+        <source src="/images/video.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
       <div className="absolute inset-0 bg-gradient-to-b from-black/40 to-transparent"></div>
     </div>
   );
